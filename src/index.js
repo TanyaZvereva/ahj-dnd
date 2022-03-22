828f000e9fda28a6670299789db0b2f55f8290de
TanyaZvereva <darkmarketgirl3@yandex.ru>
2022-03-22 20:32:50 +0300

    commit 2203

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './css/style.css'
 const firstColumn = ["Lorem ipsum dolor sit amet consectetur adipisicing elit."]
 const secondColumn = []
 const thirdColumn = []
+let draggableTask = null
+let droppableTask = null
 
 function AddCard(event, array, taskListElement) {
     const textarea = event.target.parentElement.querySelector('textarea')
@@ -13,13 +15,31 @@ function AddCard(event, array, taskListElement) {
 
 function renderColumn(column, array) {
     column.innerHTML = ''
-    array.forEach(element => {
+    array.forEach((element, index) => {
         const task =document.createElement('div')
+        task.setAttribute('draggable', 'true')
         task.classList.add('task')
+        task.addEventListener('dragstart',(event) => {
+            draggableTask=index
+        })
+        task.addEventListener('dragover',(event) => {
+            droppableTask=index
+        })
+         task.addEventListener('dragend',(event) => {
+            const temp = array[draggableTask]
+            array[draggableTask] = array[droppableTask]
+            array[droppableTask] = temp
+            renderColumn(column, array)
+        })
+        
         const text =document.createElement('span')
         text.innerText = element
         const removeBtn = document.createElement('div')
         removeBtn.innerText = 'x'
+        removeBtn.addEventListener('click', ()=> {
+           array.splice(index, 1)
+           renderColumn(column, array)
+        })
         task.append(text, removeBtn)
         column.append(task)
     });
@@ -30,8 +50,10 @@ const firstTaskList = document.querySelector('#first-column .task-list')
 firstSection.addEventListener('click', (e) => AddCard(e, firstColumn, firstTaskList))
 
 const secondtSection = document.querySelector('#second-column button')
-secondtSection.addEventListener('click', (e) => AddCard(e, secondColumn))
+const secondTaskList = document.querySelector('#second-column .task-list')
+secondtSection.addEventListener('click', (e) => AddCard(e, secondColumn, secondTaskList))
 
 const thirdSection = document.querySelector('#third-column button')
-thirdSection.addEventListener('click', (e) => AddCard(e, thirdColumn))
+const thirdTaskList = document.querySelector('#third-column .task-list')
+thirdSection.addEventListener('click', (e) => AddCard(e, thirdColumn, thirdTaskList))
 
